f0237f0b9ee723b48a00290977b7c3c7c9220c35
pisithyim <pyim@ad.oceana.org>
2017-04-01 09:22:50 -0400

    test

--- drupal8/index.php
+++ drupal8/index.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * The PHP page that serves all page requests on a Drupal installation.
+ * The PHP page that serves all page requests on a Drupal installation. awesome
  *
  * All Drupal code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
